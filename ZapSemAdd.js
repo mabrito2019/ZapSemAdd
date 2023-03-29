00f0833537c3ad9e79000d49993dc33ab734149d
Marcos Brito <tidev@eicos.com.br>
2023-03-29 14:52:51 -0300

    Html, Css e javaScript tudo junto.

--- ZapSemAdd.js
+++
@@ -1,7 +0,0 @@
-function chamar() {
-    let txtn = document.getElementById("txtn").value
-    let filtro = /\d+/g;
-    let numero = Number(txtn.match(filtro).join(''));
-    console.log(`https://wa.me/55${numero}`)
-   window.open(`https://wa.me/55${numero}`, '_blank')
-}
